1c9233a8cdbd3b2e17159477132a5e5093447f26
Matt <mgmcintyre777@gmail.com>
2020-05-14 04:21:59 -0400

    Done

--- borrower/src/main/java/com/ss/training/borrower/controller/BorrowerController.java
+++ borrower/src/main/java/com/ss/training/borrower/controller/BorrowerController.java
@@ -25,42 +25,42 @@ public class BorrowerController {
 	@Autowired
 	BorrowerService borrowerService;
 	
-	@RequestMapping(path = "/lms/borrower")
+	@RequestMapping(path = "/lms/public/borrower/borrowers")
 	public ResponseEntity<List<Borrower>> findAllBorrowers() {
 		return new ResponseEntity<List<Borrower>>(borrowerService.findAllBorrowers(), HttpStatus.OK);
 	}
 	
-	@RequestMapping(path = "/lms/borrower/{cardNo}")
+	@RequestMapping(path = "/lms/public/borrower/borrowers/{cardNo}")
 	public ResponseEntity<Borrower> findBorrower(@PathVariable Long cardNo) {
 		return new ResponseEntity<Borrower>(borrowerService.findBorrowerByCardNo(cardNo), HttpStatus.OK);
 	}
 
-	@RequestMapping(path = "/lms/borrower/{cardNo}/loans")
+	@RequestMapping(path = "/lms/public/borrower/borrowers/{cardNo}/loans")
 	public ResponseEntity<List<BookLoan>> findActiveLoans(@PathVariable Long cardNo) {
 		return new ResponseEntity<List<BookLoan>>(borrowerService.findActiveLoansByBorrower(cardNo), HttpStatus.OK);
 	}
 	
-	@RequestMapping(path = "/lms/borrower/branches")
+	@RequestMapping(path = "/lms/public/borrower/branches")
 	public ResponseEntity<List<LibraryBranch>> findAllBranches() {
 		return new ResponseEntity<List<LibraryBranch>>(borrowerService.findAllBranches(), HttpStatus.OK);
 	}
 	
-	@RequestMapping(path = "/lms/borrower/branches/{branchId}")
+	@RequestMapping(path = "/lms/public/borrower/branches/{branchId}")
 	public ResponseEntity<LibraryBranch> findBranchById(@PathVariable Long branchId) {
 		return new ResponseEntity<LibraryBranch>(borrowerService.findBranchById(branchId), HttpStatus.OK);
 	}
 
-	@RequestMapping(path = "/lms/borrower/branches/{branchId}/books")
+	@RequestMapping(path = "/lms/public/borrower/branches/{branchId}/books")
 	public ResponseEntity<List<BookCopies>> findBooksByBranch(@PathVariable Long branchId) {
 		return new ResponseEntity<List<BookCopies>>(borrowerService.findAvailibleBooksByBranch(branchId), HttpStatus.OK);
 	}
 
-	@PostMapping(path = "/lms/borrower/return")
+	@PostMapping(path = "/lms/public/borrower/borrowers/{cardNo}/loans/return")
 	public ResponseEntity<BookLoan> returnBookLoan(@RequestBody BookLoansKey id) {
 		return new ResponseEntity<BookLoan>(borrowerService.returnBookLoan(id), HttpStatus.OK);
 	}
 
-	@PostMapping(path = "/lms/borrower/{cardNo}/borrow")
+	@PostMapping(path = "/lms/public/borrower/borrowers/{cardNo}/borrow")
 	public ResponseEntity<BookLoan> borrowBookLoan(@RequestBody BookCopiesKey id, @PathVariable Long cardNo) {
 		return new ResponseEntity<BookLoan>(borrowerService.borrowBookLoan(id, cardNo), HttpStatus.OK);
 	}
